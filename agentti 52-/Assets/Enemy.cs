2c9046b502e06b8c91f01acd4ee91e00d254b09d
slimerplanet <eppuku@gmail.com>
2020-08-16 16:40:38 +0300

    first aid kit

--- agentti 52-/Assets/Enemy.cs
+++ agentti 52-/Assets/Enemy.cs
@@ -6,6 +6,7 @@ public class Enemy : MonoBehaviour
     public float health = 30;
 
     public Behaviour[] componentsToDisableOnDeath;
+    public GameObject[] objectstoSpawnOnDeath;
     public void takeDamage(float amount)
     {
         health -= amount;
@@ -17,10 +18,15 @@ public class Enemy : MonoBehaviour
     {
         Vector3 offset = new Vector3(0, 1, 0);
         transform.position += offset;
+        for (int i = 0; i < objectstoSpawnOnDeath.Length; i++)
+        {
+            Instantiate(objectstoSpawnOnDeath[i], transform.position, Quaternion.identity);
+        }
         for (int i = 0; i < componentsToDisableOnDeath.Length; i++)
         {
             componentsToDisableOnDeath[i].enabled = false;
         }
+
 
         Destroy(gameObject, 5f);
     }
---
+++ agentti 52-/Assets/healthkit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healthkit : MonoBehaviour
+{
+    public float amount;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.GetComponent<Player>() != null && other.gameObject.GetComponent<Player>().health <= 100)
+        {
+            other.gameObject.GetComponent<Player>().health += amount;
+            if (other.gameObject.GetComponent<Player>().health > 100)
+                other.gameObject.GetComponent<Player>().health = 100;
+            Destroy(gameObject);
+        }
+    }
+}
